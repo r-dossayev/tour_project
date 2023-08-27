a48e66a61ddf621f86c5b034d158ce9d6ec384f3
mallemes <dosaev.rysbek5@gmail.com>
2023-08-27 15:13:19 +0600

    refactoring project

--- app/Http/Controllers/Tour/MainController.php
+++ app/Http/Controllers/Tour/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Tour;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tour\Tour;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -10,7 +11,8 @@ use Illuminate\View\View;
 class MainController extends Controller
 {
     function index():View{
+        $re = Tour::all();
          $e= 123;
-        return view('tour.index' ,compact('e'));
+        return view('tour.index' ,compact(['e', 're']));
     }
 }
